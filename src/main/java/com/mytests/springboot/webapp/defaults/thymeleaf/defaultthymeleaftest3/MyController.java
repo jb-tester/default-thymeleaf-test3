41783eef5fe9316aaaa9d4a8ec9085c8990a1c5b
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2019-11-25 18:08:16 +0300

    add pojo class as model attribute to check the model attributes of array type resolving

--- src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest3/MyController.java
+++ src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest3/MyController.java
@@ -2,7 +2,11 @@ package com.mytests.springboot.webapp.defaults.thymeleaf.defaultthymeleaftest3;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * *******************************
@@ -17,6 +21,24 @@ public class MyController {
     public String Method(ModelMap model) {
         model.addAttribute("home_attr1", "test0");
         model.addAttribute("home_attr2", "this is default thymeleaf configuration test");
+        Person[] persons = getPeople().toArray(new Person[0]);
+        model.addAttribute("persons", persons);
         return "home";
+    }
+
+    @ModelAttribute("pa1")
+    public Person[] personsArray1(){
+        return getPeople().toArray(new Person[0]);
+    }
+    private List<Person> getPeople() {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person(1,"vasya",15,true)) ;
+        persons.add(new Person(2,"masha",10,true)) ;
+        persons.add(new Person(3,"sasha",40,false)) ;
+        persons.add(new Person(4,"dasha",40,false)) ;
+        persons.add(new Person(5,"petya",30,true)) ;
+        persons.add(new Person(6,"ivan",75,true)) ;
+        persons.add(new Person(7,"katya",20,true)) ;
+        return persons;
     }
 }
